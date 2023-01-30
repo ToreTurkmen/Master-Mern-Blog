53e48fd506704047604b6080668d855484863795
ToreTurkmen <=>
2023-01-31 00:26:51 +0100

    created static login page

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -46,4 +46,33 @@ userSchema.statics.signup = async function( email, password){
 
 }
 
+//static login
+userSchema.statics.login = async function( email, password){
+
+  //validation
+  if(!email || !password){
+
+    throw Error('All fields must be ')
+  }
+  
+
+
+  const user = await this.findOne({ email })
+
+  if(!user) {
+    throw Error('Incorrect email')
+  }
+
+  const match = await bcrypt.compare(password, user.password)
+
+  if(!match) {
+    throw Error('Incorrect password')
+  }
+  
+
+  return user
+
+}
+
+
 module.exports = mongoose.model('User', userSchema)
